adca10e0ff1cb0d713a9b909adb90be24866113f
c0d3Gems <techdubberstudio@gmail.com>
2021-10-08 09:00:55 +0300

    try to change something

--- _app/pages/cauta/[query].svelte-2b5fe513.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as n}from"../../chunks/vendor-e9a62dbe.js";class u extends s{constructor(e){super();t(this,e,null,null,n,{})}}export{u as default};
--- _app/pages/scoli/top-100-national.svelte-fb266742.js
+++
@@ -1,1 +0,0 @@
-import{S as e,i as n,s as t}from"../../chunks/vendor-e9a62dbe.js";class l extends e{constructor(s){super();n(this,s,null,null,t,{})}}export{l as default};
